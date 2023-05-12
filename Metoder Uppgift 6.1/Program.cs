f43054dbd84b8f52b1517b5eb3def8aa3ae1292b
Admin <Admin@TEB127006>
2023-05-12 08:49:04 +0200

    Add project files.

---
+++ Metoder Uppgift 6.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+
+    static void Main()
+    {
+        Addera(1, 2, 3);
+
+
+    }
+
+    static void Addera(int tal1, int tal2, int tal3)
+    {
+        int Summa = (tal1 + tal2 + tal3);
+        Console.WriteLine(Summa);
+
+    }
+
+}
